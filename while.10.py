df737ef38c8ae3920e4cd965d20cd30ec1e3580f
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-02-25 11:25:03 +0000

    Add files via upload

---
+++ while.10.py
@@ -0,0 +1,12 @@
+n = int(input("enter the number: "))
+while n<=0:
+   n = int(input("enter the number: ")) 
+for i in range (1,n+1):
+    if (n % i == 0):
+        print ("the devisible ",i)
+
+   
+
+
+
+
